10a38390964e4e0b803a180b570821a7f78fd75b
Fabiano Moraes <142937939+Fab-Moraes@users.noreply.github.com>
2023-10-08 08:24:27 -0300

    Update Desafio.java

--- Desafio.java
+++ Desafio.java
@@ -8,24 +8,21 @@ public class Desafio {
      * @param args
      */
     public static void main(String[] args){
-        //Lê os valores de Entrada:
+        
         Scanner leitorDeEntradas = new Scanner(System.in);
         float valorSalario = leitorDeEntradas.nextFloat();
         float valorBeneficios = leitorDeEntradas.nextFloat();
 
         float valorImposto = 0;
         if (valorSalario >= 0 && valorSalario <= 1100) {
-            //Atribuiu a aliquota de 5% mediante o salário:
             valorImposto = 0.05F * valorSalario;
         }
         
         if (valorSalario >= 1100.01 && valorSalario <= 2500.00) {
-            //Atribuiu a aliquota de 10% mediante o salário:
             valorImposto = 0.10F * valorSalario;
         }
 
         if (valorSalario > 2500.00) {
-            //Atribuiu a aliquota de 15% mediante o salário:
             valorImposto = 0.15F * valorSalario;
         }
 
